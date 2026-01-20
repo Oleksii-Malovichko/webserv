97279b923f7da00aa274588e79b1a0b5a0c37463
Tamas Farkas <farkas0904@gmail.com>
2026-01-20 15:43:04 +0100

    HttpError class made with the client and server error codes

--- error_handler/errorHandler.hpp
+++ error_handler/errorHandler.hpp
@@ -2,8 +2,10 @@
 # define ERREOR_HANDLER_HPP
 
 # include <errno.h>
+# include <unistd.h>
 # include <string>
 # include <iostream>
+# include <sstream>
 # include <cstring>
 
 # define RED "\033[1;31m"
@@ -14,22 +16,65 @@
 # define CYAN "\033[1;36m"
 # define DEFAULT "\033[0m"
 
+class httpErrors
+{
+	private:
+		std::string _reason;
+		std::string _body;
+		bool close_connection;
+	
+	public:
+		httpErrors(void);
+		httpErrors(const int status_code);
+		httpErrors(const httpErrors& other);
+		httpErrors& operator=(const httpErrors& other);
+		~httpErrors(void);
+
+		static const std::string reasonPhrase(int status_code);
+		static const std::string errorBody(int status_code);
+
+		// Client Errors
+		void error400(void);
+		void error403(void);
+		void error404(void);
+		void error405(void);
+		void error408(void);
+		void error413(void);
+		void error414(void);
+
+		// Server Errors
+		void error500(void);
+		void error502(void);
+		void error504(void);
+		void error505(void);
+
+		const std::string& getReason(void) const;
+		const std::string& getBody(void) const;
+		const bool& getCloseConnection(void) const;
+};
+
 class errorHandler
 {
 	private:
 		int _error_code;
 		std::string _message;
+		httpErrors _http_err_response;
+
+		//std::string _reason;
+		//std::string _body;
 
 		errorHandler(void);
 	
 	public:
 		errorHandler(const std::string& text);
+		errorHandler(const int status_code);
 		errorHandler(const errorHandler& other);
 		errorHandler& operator=(const errorHandler& other);
 		~errorHandler(void);
 
 		void printStderr(void);
 		void printStdout(void);
+		void printFd(const int fd);
 
 		int getErrorCode(void) const;
 		
--- error_handler/test_errorHandler.cpp
+++ error_handler/test_errorHandler.cpp
@@ -5,7 +5,8 @@
 
 int main(void)
 {
-	int fd = open("non_existent_file.txt", O_RDONLY);
+	int fd = open("try.txt", O_RDONLY);
+	//int fd = open("non_existent_file.txt", O_RDONLY);
 	if (fd == -1)
 	{
 		errorHandler error_msg("Failed to open file");
@@ -14,5 +15,11 @@ int main(void)
 		return (error_code);
 	}
 
+	errorHandler test1(100);
+	test1.printFd(STDERR_FILENO);
+	errorHandler test2(400);
+	test2.printFd(STDERR_FILENO);
+
+	close(fd);
 	return (0);
 }
